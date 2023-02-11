42b1221e26bf5735c79f3cf5736cfb3c7039dd3a
Sherlock <sherlockh989@gmail.com>
2023-02-11 12:28:53 +0200

    Added data class Copy data class Added Example Cycles

---
+++ app/src/main/java/com/sherlock/gb/kotlin/hws/hw1/DataClassTest.kt
@@ -0,0 +1,3 @@
+package com.sherlock.gb.kotlin.hws.hw1
+
+data class DataClassTest(val param1: String, val param2: Int)
--- app/src/main/java/com/sherlock/gb/kotlin/hws/hw1/MainActivity.kt
+++ app/src/main/java/com/sherlock/gb/kotlin/hws/hw1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.sherlock.gb.kotlin.hws.hw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import com.sherlock.gb.kotlin.hws.hw1.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +13,29 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding!!
 
+    private val dataClassTest = DataClassTest("dddd",5)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.param1.text = dataClassTest.param1
+        binding.param2.text = dataClassTest.param2.toString()
+
         binding.button1.setOnClickListener {
-            Toast.makeText(this, "COPY", Toast.LENGTH_SHORT).show()
+            val dataClassTestCopy = dataClassTest.copy(dataClassTest.param1,6)
+            binding.param1Copy.text = dataClassTestCopy.param1
+            binding.param2Copy.text = dataClassTestCopy.param2.toString()
+        }
+
+        binding.button2.setOnClickListener {
+            for(i in 1..10) {
+                Log.d(TAG, "i:$i")
+            }
+            repeat(10){
+                Log.d(TAG, "Хочу денег " + (it + 1)+ " раз!!!")
+            }
         }
     }
 
